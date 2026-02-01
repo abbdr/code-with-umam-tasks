d3e920efbeab0fd9e22bbf96d63d599d257df2e2
Rozag <www.rozag@gmail.com>
2026-02-02 01:28:36 +0700

    task 2

--- task2/models/models.go
+++ task2/models/models.go
@@ -2,7 +2,7 @@ package models
 
 type Product struct {
 	ID         int    `json:"id"`
-	Name       string `json:"name"`
+	Name       string `json:"product_name"`
 	Price      int    `json:"price"`
 	Stock      int    `json:"stock"`
 	CategoryId int    `json:"category_id"`
