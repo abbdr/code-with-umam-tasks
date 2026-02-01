7f467be448d6334a336cdd5833a79ffa3cfecc89
Rozag <www.rozag@gmail.com>
2026-02-02 00:41:22 +0700

    task 2 mvp

--- task2/models/models.go
+++ task2/models/models.go
@@ -1,10 +1,11 @@
 package models
 
 type Product struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Price int    `json:"price"`
-	Stock int    `json:"stock"`
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Price      int    `json:"price"`
+	Stock      int    `json:"stock"`
+	CategoryId int    `json:"category_id"`
 }
 
 type Category struct {
